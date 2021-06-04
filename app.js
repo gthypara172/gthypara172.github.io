090810544f099e6a5c96c6ab0440b9f5fbdc97ce
gthypara172 <80452986+gthypara172@users.noreply.github.com>
2021-06-04 10:34:32 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,14 @@
-const secondHand = document.querySelector(".second-hand");
-const minuteHand = document.querySelector(".min-hand");
-const hourHand = document.querySelector(".hour-hand");
+const inputs = document.querySelectorAll(".controls input");
 
-function setDate() {
-    const now = new Date();
-    const seconds = now.getSeconds();
 
-    const secondsDegrees = ((seconds / 60) * 360) + 90;
-    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-    console.log(seconds);
 
-    const minutes = now.getMinutes();
+function handleUpdate() {
+    const suffix = this.dataset.sizing || "";
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
 
-    const minutesDegrees = ((minutes / 60) * 360) + 90;
-    minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
-    console.log(minutes);
-
-    const hours = now.getHours();
-
-    const hoursDegrees = ((hours / 12) * 360) + 90;
-    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
-    console.log(hours);
 };
 
 
-setInterval(setDate, 1000);
+
+inputs.forEach(input => input.addEventListener("change", handleUpdate));
+inputs.forEach(input => input.addEventListener("mousemove", handleUpdate));
