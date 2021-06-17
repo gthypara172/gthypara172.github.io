23ed88cf04c5ae66bce76b4b13455022f6c777da
gthypara172 <80452986+gthypara172@users.noreply.github.com>
2021-06-17 12:33:25 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,61 +1,27 @@
-let randomNumber = Math.trunc(Math.random() * 20) + 1;
+const secondHand = document.querySelector(".second-hand");
+const minuteHand = document.querySelector(".min-hand");
+const hourHand = document.querySelector(".hour-hand");
+
+function setDate() {
+    const now = new Date();
+    const seconds = now.getSeconds();
+
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    //console.log(seconds);
+
+    const minutes = now.getMinutes();
+
+    const minutesDegrees = ((minutes / 60) * 360) + 90;
+    minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+    //console.log(minutes);
+
+    const hours = now.getHours();
+
+    const hoursDegrees = ((hours / 12) * 360) + 90;
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+    //console.log(hours);
+};
 
 
-let score = 10;
-let highScore = 0;
-
-
-document.querySelector(".check").addEventListener("click", function() {
-    const guess = Number(document.querySelector(".guess").value);
-
-    //When there is no input
-    if (!guess) {
-        document.querySelector(".message").textContent = "No Guess";
-        //When player wins
-    } else if (guess === randomNumber) {
-        document.querySelector(".message").textContent = "Correct Guess";
-        document.querySelector(".number").textContent = randomNumber;
-        document.querySelector("body").style.backgroundColor = "#BBBE25";
-        document.querySelector(".number").style.width = "30rem";
-        if (score > highScore) {
-            highScore = score;
-            document.querySelector(".highscore").textContent = highScore;
-        };
-
-
-        //When guess is too high
-    } else if (guess > randomNumber) {
-
-        if (score > 1) {
-            document.querySelector(".message").textContent = "Guess is too high";
-            score--;
-            document.querySelector(".score").textContent = score;
-        } else {
-            document.querySelector(".message").textContent = "Game Over, You lose";
-            document.querySelector(".score").textContent = 0;
-        }
-        //When guess is too low
-    } else if (guess < randomNumber) {
-        if (score > 1) {
-            document.querySelector(".message").textContent = "Guess is too low";
-            score--;
-            document.querySelector(".score").textContent = score;
-        } else {
-            document.querySelector(".message").textContent = "Game Over, You lose";
-            document.querySelector(".score").textContent = 0;
-        };
-    };
-
-});
-
-document.querySelector(".again").addEventListener("click", function() {
-    score = 10;
-    randomNumber = Math.trunc(Math.random() * 20) + 1;
-    document.querySelector(".message").textContent = "Start Guessing";
-    document.querySelector(".score").textContent = score;
-    document.querySelector(".number").textContent = "?";
-    document.querySelector(".guess").value = "";
-    document.querySelector("body").style.backgroundColor = "#1c4479";
-    document.querySelector(".number").style.width = "15rem";
-
-});
+setInterval(setDate, 1000);
